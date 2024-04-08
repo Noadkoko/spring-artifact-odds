15f5001e517a222343cc22da102705cd7448259c
Huy-Hai Vincent NGUYEN <nhhvincent@gmail.com>
2024-04-08 13:20:44 +0200

    first commit

---
+++ src/main/java/fr/khry/springartifactodds/SpringArtifactOddsApplication.java
@@ -0,0 +1,13 @@
+package fr.khry.springartifactodds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringArtifactOddsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringArtifactOddsApplication.class, args);
+	}
+
+}
